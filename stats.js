c310e9e3038344581c406648d931d22c0717d1b3
Richard <ryaanx@live.com>
2021-02-14 22:30:56 -0300

    Stats finalizado

--- stats.js
+++ stats.js
@@ -1,6 +1,26 @@
 const os = require('os')
 // console.log(os.platform())
 
+setInterval(() =>{
+
 const {freemem, totalmem} = os
 
-console.log(freemem(), totalmem())
+// console.log(`${parseInt(freemem() / 1024 / 1024)} MB disponíveis de` , `${parseInt(totalmem()/1024/1024)} MB`)
+
+const total = parseInt(totalmem() / 1024 / 1024)
+const mem = parseInt(freemem() / 1024 / 1024)
+
+const percents = parseInt(100-((mem / total) * 100))
+
+// console.log(mem, total, percents)
+
+const stats = {
+    free: `${mem} MB`,
+    total:`${total} MB`,
+    usage:`${percents} %`
+}
+console.clear()
+console.log("=== PC Stats ===")
+console.table(stats)
+//console.log(stats)
+},1000)
